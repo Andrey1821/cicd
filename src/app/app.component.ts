0924f120d4d6cc91c055ebfc37bc53478ef99ddf
andrey <andrey.chuprina@agilie.com>
2023-07-21 00:51:46 +0300

    update workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,38 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'cicd';
+  // private proxy = new Proxy<any>(({
+  //   getValue(): any {
+  //     const {getValue, ...value} = {...this} as any;
+  //     return value;
+  //   }
+  // } as any), {
+  //   set: (target, prop, value) => {
+  //     target[prop] = value;
+  //     this.proxySetEffect();
+  //     return true;
+  //   },
+  //   get(target: any, prop: string | symbol): any {
+  //     const value = target[prop];
+  //     if(typeof value === 'function') {
+  //       return value.bind(target);
+  //     }
+  //     return target[prop];
+  //   }
+  // })
 
-  constructor(private router: Router) {}
+  private promise = new Promise(() => {});
+
+  constructor(private router: Router) {
+    this.example();
+  }
+
+  private example(): void {
+    // setTimeout(() => this.proxy.age = 4, 3000)
+  }
+
+  private proxySetEffect(): void {
+    // console.log(this.proxy.getValue());
+    // console.log(this.proxy.age);
+  }
 }
